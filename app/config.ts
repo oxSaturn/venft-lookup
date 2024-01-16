180d2dd2728c810970b3d0a57a4f6dd83cda3680
oxSaturn <oxSaturn@proton.me>
2024-01-16 19:32:37 +0800

    add code

---
+++ app/config.ts
@@ -0,0 +1,82 @@
+import {
+  type Chain,
+  fantom,
+  base,
+  optimism,
+  arbitrum,
+  avalanche,
+  mantle,
+} from "viem/chains";
+import { type Abi } from "viem";
+import { abi } from "./abis/abi1";
+import { abi as abi2 } from "./abis/abi2";
+export const config: Record<
+  string,
+  {
+    chain: Chain;
+    address: `0x${string}`;
+    abi: Abi;
+    underlying: `0x${string}`; // to get price from dexscreener
+  }
+> = {
+  veFVM: {
+    chain: fantom,
+    address: "0xae459ee7377fb9f67518047bba5482c2f0963236",
+    abi: abi,
+    underlying: "0x07BB65fAaC502d4996532F834A1B7ba5dC32Ff96",
+  },
+  veBVM: {
+    chain: base,
+    address: "0x91F85d68B413dE823684c891db515B0390a02512",
+    abi: abi,
+    underlying: "0xd386a121991E51Eab5e3433Bf5B1cF4C8884b47a",
+  },
+  veVelo: {
+    chain: optimism,
+    address: "0xFAf8FD17D9840595845582fCB047DF13f006787d",
+    abi: abi2,
+    underlying: "0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db",
+  },
+  veAero: {
+    chain: base,
+    address: "0xebf418fe2512e7e6bd9b87a8f0f294acdc67e6b4",
+    abi: abi2,
+    underlying: "0x940181a94A35A4569E4529A3CDfB74e38FD98631",
+  },
+  veRAM: {
+    chain: arbitrum,
+    address: "0xaaa343032aa79ee9a6897dab03bef967c3289a06",
+    abi,
+    underlying: "0xAAA6C1E32C55A7Bfa8066A6FAE9b42650F262418",
+  },
+  veEqual: {
+    chain: fantom,
+    address: "0x8313f3551c4d3984ffbadfb42f780d0c8763ce94",
+    abi,
+    underlying: "0x3Fd3A0c85B70754eFc07aC9Ac0cbBDCe664865A6",
+  },
+  vePhar: {
+    chain: avalanche,
+    address: "0xAAAEa1fB9f3DE3F70E89f37B69Ab11B47eb9Ce6F",
+    abi,
+    underlying: "0xAAAB9D12A30504559b0C5a9A5977fEE4A6081c6b",
+  },
+  veCleo: {
+    chain: mantle,
+    address: "0xAAAEa1fB9f3DE3F70E89f37B69Ab11B47eb9Ce6F",
+    abi,
+    underlying: "0xC1E0C8C30F251A07a894609616580ad2CEb547F2",
+  },
+  veScale: {
+    chain: base,
+    address: "0x28c9c71c776a1203000b56c0cca48bef1cd51c53",
+    abi,
+    underlying: "0x54016a4848a38f257B6E96331F7404073Fd9c32C",
+  },
+  veHRA: {
+    chain: arbitrum,
+    address: "0x44ccA4FB1737F6A5DEb2AC1Bc1F3D4075bBF9db4",
+    abi,
+    underlying: "0xE594b57E7F11ec1E8Af9f003F74Fa52B7aefdc9F",
+  },
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,32 +1,256 @@
 import type { MetaFunction } from "@remix-run/node";
+import {
+  Form,
+  useSubmit,
+  useSearchParams,
+  useLoaderData,
+  ClientLoaderFunctionArgs,
+  useNavigation,
+} from "@remix-run/react";
+import { config } from "../config";
+import { createPublicClient, http, formatUnits, zeroAddress } from "viem";
+import { abi } from "~/abis/abi1";
+import { abi as abi2 } from "~/abis/abi2";
+import { Spinner } from "~/components/Spinner";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix SPA" },
-    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+    { title: "veNFT" },
+    { name: "description", content: "Lookup veNFT data!" },
   ];
 };
 
+async function getCoinPrice(token: string): Promise<number> {
+  try {
+    const res = await fetch(
+      `https://api.dexscreener.com/latest/dex/tokens/${token}`
+    )
+      .then((resp) => resp.json())
+      .then(({ pairs }) => pairs[0].priceUsd * 1);
+    return res;
+  } catch (e) {
+    return 0;
+  }
+}
+
+export async function clientLoader({
+  request,
+}: ClientLoaderFunctionArgs): Promise<null | {
+  id: string;
+  balance: string;
+  amount: string;
+  end: bigint;
+  owner: string;
+  price: number;
+}> {
+  const url = new URL(request.url);
+  const venft = url.searchParams.get("venft");
+  const id = url.searchParams.get("id");
+  let client;
+  if (venft && id) {
+    const { chain, address, abi: venftAbi, underlying } = config[venft];
+    client = createPublicClient({
+      chain,
+      transport: http(),
+    });
+    const [balance, decimals, owner] = await client.multicall({
+      allowFailure: false,
+      contracts: [
+        {
+          address,
+          abi: abi,
+          functionName: "balanceOfNFT",
+          args: [BigInt(id)],
+        },
+        {
+          address,
+          abi: abi,
+          functionName: "decimals",
+        },
+        {
+          address,
+          abi: abi,
+          functionName: "ownerOf",
+          args: [BigInt(id)],
+        },
+      ],
+    });
+    const price = await getCoinPrice(underlying);
+    if (venftAbi === abi2) {
+      const [data] = await client.multicall({
+        allowFailure: false,
+        contracts: [
+          {
+            address,
+            abi: abi2,
+            functionName: "locked",
+            args: [BigInt(id)],
+          },
+        ],
+      });
+      return {
+        id: id,
+        balance: formatUnits(balance, decimals),
+        amount: formatUnits(data.amount, decimals),
+        end: data.end,
+        owner,
+        price,
+      };
+    }
+    if (venftAbi === abi) {
+      const [data] = await client.multicall({
+        allowFailure: false,
+        contracts: [
+          {
+            address,
+            abi,
+            functionName: "locked",
+            args: [BigInt(id)],
+          },
+        ],
+      });
+
+      return {
+        id: id,
+        balance: formatUnits(balance, decimals),
+        amount: formatUnits(data[0], decimals),
+        end: data[1],
+        owner,
+        price,
+      };
+    }
+  }
+  return null;
+}
+
 export default function Index() {
+  const submit = useSubmit();
+  const navigation = useNavigation();
+  const [searchParams] = useSearchParams();
+  const venft = searchParams.get("venft");
+  const data = useLoaderData<typeof clientLoader>();
+  const groupedByChain = Object.entries(config).reduce((acc, [key, value]) => {
+    if (!acc[value.chain.name]) {
+      acc[value.chain.name] = [];
+    }
+    acc[value.chain.name].push(key);
+    return acc;
+  }, {} as Record<string, string[]>);
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/future/spa-mode"
-            rel="noreferrer"
+    <div className="mx-5">
+      <h1 className="text-3xl my-5">
+        Lookup {venft ? `${venft} ` : "veNFT"} data
+      </h1>
+      <Form method="get" action="/" className="grid gap-y-6">
+        <div className="flex flex-wrap gap-3">
+          {Object.entries(groupedByChain).map(([chainName, value]) => (
+            <fieldset
+              key={chainName}
+              className="border border-gray-500 border-solid p-3 rounded-md"
+            >
+              <legend className="bg-black text-white px-3">{chainName}</legend>
+              {value.map((key) => (
+                <div key={key} className="flex items-center gap-x-1">
+                  <input
+                    id={key}
+                    type="radio"
+                    name="venft"
+                    value={key}
+                    defaultChecked={venft === key}
+                    onChange={(e) => submit(e.currentTarget.form)}
+                  />
+                  <label htmlFor={key}>{key}</label>
+                </div>
+              ))}
+            </fieldset>
+          ))}
+        </div>
+        <div>
+          <label htmlFor="id" className="flex gap-x-1 items-center">
+            <span className="">NFT ID:</span>
+            <input
+              name="id"
+              type="number"
+              className="border border-gray-500 rounded-md px-2 py-2"
+              onChange={(e) => submit(e.currentTarget.form)}
+              defaultValue={data?.id || ""}
+            />
+          </label>
+        </div>
+
+        {data ? (
+          <div
+            className={`${
+              navigation.state === "loading" ? "animate-pulse" : ""
+            } relative border-solid rounded-lg border-black border-2 py-3 px-3`}
           >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+            {navigation.state === "loading" ? (
+              <div className="absolute bg-gray-500/50 animate-pulse left-0 top-0 right-0 bottom-0 flex items-center justify-center">
+                <Spinner />
+              </div>
+            ) : null}
+            <div className="space-y-3 sm:space-y-1">
+              <div className="flex flex-col sm:flex-row">
+                <span className="font-bold w-40 inline-block text-base">
+                  {venft} ID:
+                </span>{" "}
+                <span>{data.id}</span>
+              </div>
+              <div className="flex flex-col sm:flex-row">
+                <span className="font-bold w-40 inline-block text-base">
+                  Owner:
+                </span>{" "}
+                <span>
+                  {data.owner === zeroAddress ? (
+                    data.owner
+                  ) : (
+                    <a
+                      href={`https://debank.com/profile/${data.owner}`}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="underline text-blue-500 break-all"
+                    >
+                      {data.owner}
+                    </a>
+                  )}
+                </span>
+              </div>
+              <div className="flex flex-col sm:flex-row">
+                <span className="font-bold w-40 inline-block text-base">
+                  {venft?.replace("ve", "")} Locked:
+                </span>{" "}
+                <span>{(+data.amount).toLocaleString()}</span>
+                {data.price && data.amount !== "0" ? (
+                  <span>(${(data.price * +data.amount).toLocaleString()})</span>
+                ) : null}
+              </div>
+              <div className="flex flex-col sm:flex-row">
+                <span className="font-bold  w-40 inline-block">
+                  {venft} Balance:
+                </span>{" "}
+                <span>{(+data.balance).toLocaleString()}</span>
+              </div>
+              <div className="flex flex-col sm:flex-row">
+                <span className="font-bold  w-40 inline-block">Unlock:</span>{" "}
+                <span>
+                  {data.end !== 0n
+                    ? new Date(Number(data.end) * 1000).toUTCString()
+                    : "-"}
+                </span>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </Form>
+      <footer className="text-center text-black/50 text-xs">
+        Built by{" "}
+        <a
+          href="https://github.com/oxSaturn"
+          className="underline text-blue-500"
+        >
+          oxSaturn
+        </a>
+      </footer>
     </div>
   );
 }
